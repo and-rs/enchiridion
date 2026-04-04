e59ce28ad2f94deb5aa23d524acf93beca4a08cb
and-rs <deserd@protonmail.com>
2026-04-04 13:40:20 -0500

    feat: expand Nix flake to support multi-platform systems
    - Define supportedSystems for Linux and Darwin architectures
    - Use genAttrs to iterate over all system configurations
    - Abstract pkgs generation into reusable function
    - Add automatic opam initialization in shellHook

--- flake.nix
+++ flake.nix
@@ -5,39 +5,57 @@
   outputs =
     { self, nixpkgs }:
     let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgsFor = system: import nixpkgs { inherit system; };
     in
     {
-      devShells.${system}.default = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
-        packages = with pkgs; [
-          llvmPackages.libcxx
-          llvmPackages.llvm
-          pkg-config
-          autoconf
-          openssl
-          libffi
-          clang
-          opam
-          dune
-          gmp
-        ];
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = pkgsFor system;
+        in
+        {
+          default = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
+            packages = with pkgs; [
+              llvmPackages.libcxx
+              llvmPackages.llvm
+              pkg-config
+              autoconf
+              openssl
+              libffi
+              clang
+              opam
+              dune
+              gmp
+            ];
 
-        shellHook = ''
-          export CC=${pkgs.clang}/bin/clang
-          export CXX=${pkgs.clang}/bin/clang++
-          export CFLAGS="-Wno-error=int-conversion -Wno-error=incompatible-pointer-types"
+            shellHook = ''
+              export CC=${pkgs.clang}/bin/clang
+              export CXX=${pkgs.clang}/bin/clang++
+              export CFLAGS="-Wno-error=int-conversion -Wno-error=incompatible-pointer-types"
 
-          if [ -z "$LIBRARY_PATH" ]; then
-            export LIBRARY_PATH=${pkgs.llvmPackages.libcxx}/lib
-          else
-            export LIBRARY_PATH=${pkgs.llvmPackages.libcxx}/lib:$LIBRARY_PATH
-          fi
+              if [ -z "$LIBRARY_PATH" ]; then
+                export LIBRARY_PATH=${pkgs.llvmPackages.libcxx}/lib
+              else
+                export LIBRARY_PATH=${pkgs.llvmPackages.libcxx}/lib:$LIBRARY_PATH
+              fi
 
-          export AR=${pkgs.llvmPackages.llvm}/bin/llvm-ar
-          export NM=${pkgs.llvmPackages.llvm}/bin/llvm-nm
-          export RANLIB=${pkgs.llvmPackages.llvm}/bin/llvm-ranlib
-        '';
-      };
+              export AR=${pkgs.llvmPackages.llvm}/bin/llvm-ar
+              export NM=${pkgs.llvmPackages.llvm}/bin/llvm-nm
+              export RANLIB=${pkgs.llvmPackages.llvm}/bin/llvm-ranlib
+
+              if [ ! -d "$HOME/.opam" ]; then
+                opam init --bare --no-setup -y
+              fi
+            '';
+          };
+        }
+      );
     };
 }
